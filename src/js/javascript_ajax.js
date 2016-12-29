ea3d2bcca3f2002c55066548310742bcfcfa1336
Shankar Thiyagaraajan <shankarThiyagaraajan@users.noreply.github.com>
2016-12-29 12:02:07 +0530

    - Initial Commit

---
+++ src/js/javascript_ajax.js
@@ -0,0 +1,62 @@
+// Simple Ajax Object
+var ajax = {
+    x:function () {
+    if (typeof XMLHttpRequest !== 'undefined') {
+        return new XMLHttpRequest();
+    }
+    var versions = [
+        "MSXML2.XmlHttp.6.0",
+        "MSXML2.XmlHttp.5.0",
+        "MSXML2.XmlHttp.4.0",
+        "MSXML2.XmlHttp.3.0",
+        "MSXML2.XmlHttp.2.0",
+        "Microsoft.XmlHttp"
+    ];
+
+    var xhr;
+    for (var i = 0; i < versions.length; i++) {
+        try {
+            xhr = new ActiveXObject(versions[i]);
+            break;
+        } catch (e) {
+        }
+    }
+    return xhr;
+   },
+
+  send:function (url, callback, method, data, async) {
+    if (async === undefined) {
+        async = true;
+    }
+    var x = this.x();
+    x.open(method, url, async);
+    x.onreadystatechange = function () {
+        if (x.readyState == 4) {
+            callback(x.responseText)
+        }
+    };
+    if (method == 'POST') {
+        x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    }
+    x.send(data)
+},
+
+   // For GET Request.
+  get:function (url, data, callback, async) {
+    var query = [];
+    for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    }
+    this.send(url + (query.length ? '?' + query.join('&') : ''), callback, 'GET', null, async)
+},
+
+   // For POST Request.
+   post:function (url, data, callback, async) {
+    var query = [];
+    for (var key in data) {
+        query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
+    }
+    this.send(url, callback, 'POST', query.join('&'), async)
+}
+
+};
